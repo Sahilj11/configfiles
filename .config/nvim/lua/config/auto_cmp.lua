76ed5d73aa7bba393f68f98445957e1e30374451
sahil jalandhara <sahiljalandhara123@gmail.com>
2025-04-13 15:56:31 +0530

    reset mapping

--- .config/nvim/lua/config/auto_cmp.lua
+++ .config/nvim/lua/config/auto_cmp.lua
@@ -25,7 +25,7 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ["<C-e>"] = cmp.mapping(function(fallback)
+        ["<CR>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 if luasnip.expand() then
                     luasnip.expand()
