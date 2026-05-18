c13299e71f3eb7c7fa3665fa49e9c40f26343d14
Sahil Jalandhara <sahiljalandhara123@gmail.com>
2026-05-18 20:43:29 +0530

    changing neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,10 @@ require("settings.options")
 require("settings.keymaps")
 require("config.lazy")
 require("config.lsp")
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,45 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  -- Removing lazy = false or setting it to true allows lazy.nvim to properly 
-  -- sequence the loading order so 'nvim-treesitter.configs' can actually be found.
-  lazy = false, 
+  lazy = false,
   build = ":TSUpdate",
-  config = function()
-    -- Safe require: wrapping it ensures that even if something is sequence-broken,
-    -- it won't hard-crash your entire Neovim configuration on startup.
-    local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
-    if not status_ok then
-      return
-    end
-
-    treesitter_configs.setup({
-      -- A list of parser names, or "all"
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "java",
-        "json",
-        "javascript",
-        "python",
-        "css",
-        "typescript",
-        "svelte", -- Added svelte here so it automatically installs
-      },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end,
 }
